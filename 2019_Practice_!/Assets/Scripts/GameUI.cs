3d595ffde1dc956381041949abb9dc8bd4a0e221
Vishesh Kumar <vishesh@visheshk.net>
2019-03-18 17:05:05 -0500

    adding my code

--- 2019_Practice_!/Assets/Scripts/GameUI.cs
+++ 2019_Practice_!/Assets/Scripts/GameUI.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// This should be a per player UI and score controller
+//I would rename this script to TeamController, or maybe just move my code edits to a TeamController and attach that to a team prefab
+
 public class GameUI : MonoBehaviour
 {
+
+    /*list of producers */
     [SerializeField] private Text Current_People;
     [SerializeField] private Text Current_Food;
     [SerializeField] private Text Current_Pollution;
@@ -15,6 +20,9 @@ public class GameUI : MonoBehaviour
     [SerializeField] private Text Current_Farm;
     [SerializeField] private Text Current_Park;
 
+    private Dictionary<string, int> teamResources;
+
+    List<Producer> producersOwned;
     private int Gold_InitialScore = 10;
     private int Metal_InitialScore = 10;
     private int People_InitialScore = 3;
@@ -24,7 +32,7 @@ public class GameUI : MonoBehaviour
     private int Factory_InitialScore = 0;
     private int Farm_InitialScore = 0;
     private int Park_InitialScore = 0;
-
+    //Make most references to the values of resources at the dictionary level rather than individual variables
     void Start()
     {
         People_DispScore(0);
@@ -36,7 +44,31 @@ public class GameUI : MonoBehaviour
         Factory_DispScore(0);
         Farm_DispScore(0);
         Park_DispScore(0);
+        teamResources = new Dictionary<string, int>() {
+            {"gold", 10},
+            {"metal", 10},
+            {"food", 5},
+            {"cotton", 5}
+        };
     }
+
+    public void BuyProducer(Dictionary<string, int> prodCost) {
+         foreach (string key in prodCost.Keys) {
+             teamResources[key] = teamResources[key] - prodCost[key];
+         }
+    }
+
+    public void EndRound() {
+        foreach(Producer prod in producersOwned) {
+            foreach (string key in prod.runCost.Keys) {
+                teamResources[key] = teamResources[key] - prod.runCost[key];
+            }
+            foreach (string key in prod.runProduction.Keys) {
+                teamResources[key] = teamResources[key] + prod.runProduction[key];
+            }
+        }
+    }
+
 
     public void People_DispScore(int PeopleScore)
     {
---
+++ 2019_Practice_!/Assets/Scripts/Producer.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Producer : MonoBehaviour
+{
+    public string kind;     //kind can be factory, farm, park
+    //one time costs at the start
+    public int peopleEmployed;
+    public Dictionary<string, int> startCost;
+
+    //per round costs
+    public Dictionary<string, int> runCost;
+    public Dictionary<string, int> runProduction;
+    public int pollutionMade;
+
+    public Dictionary<string, Dictionary<string, int>> runCosts = new Dictionary<string, Dictionary<string, int>> {
+        {"gold", new Dictionary<string, int> { 
+            {"metal", 1},
+            {"cotton", 2},
+            {"people", 1}
+         }
+        },
+        {"metal", new Dictionary<string, int> { 
+            {"gold", 1},
+            {"food", 2},
+            {"people", 1}
+         }
+        },
+        {"cotton", new Dictionary<string, int> { 
+            {"metal", 2},
+            {"food", 1},
+            {"people", 1}
+         }
+        },
+        {"food", new Dictionary<string, int> { 
+            {"gold", 2},
+            {"cotton", 1},
+            {"people", 1}
+         }
+        },
+        {"park", new Dictionary<string, int> { 
+            {"metal", 1},
+            {"cotton", 1},
+            {"food", 1},
+            {"people", 2}
+         }
+        }
+    };
+
+    /*
+    public Dictionary<string, Dictionary<string, int>> prodCosts = new Dictionary<string, Dictionary<string, int>> {
+        {"gold", new Dictionary<string, int> { 
+            {"metal", 1}
+         }
+        },
+        {"metal", new Dictionary<string, int> { 
+            {"gold", 1},
+            {"food", 2},
+            {"people", 1}
+         }
+        },
+        {"cotton", new Dictionary<string, int> { 
+            {"metal", 2},
+            {"food", 1},
+            {"people", 1}
+         }
+        },
+        {"food", new Dictionary<string, int> { 
+            {"gold", 2},
+            {"cotton", 1},
+            {"people", 1}
+         }
+        },
+        {"park", new Dictionary<string, int> { 
+            {"metal", 1},
+            {"cotton", 1},
+            {"food", 1},
+            {"people", 2}
+         }
+        }
+    };
+    */
+
+    public Dictionary<string, Dictionary<string, int>> runProd = new Dictionary<string, Dictionary<string, int>> {
+        {"gold", new Dictionary<string, int> { 
+            {"gold", 1},
+            {"pollution", 2}
+        }
+        },
+        {"metal", new Dictionary<string, int> { 
+            {"metal", 1},
+            {"pollution", 1}
+         }
+        },
+        {"cotton", new Dictionary<string, int> { 
+            {"cotton", 1},
+            {"pollution", 1}
+         }
+        },
+        {"food", new Dictionary<string, int> { 
+            {"food", 1},
+            {"pollution", 1}
+         }
+        },
+        {"park", new Dictionary<string, int> { 
+            // {"food", 1},
+            {"pollution", -2}
+         }
+        }
+    };
+
+    public Producer(string kd, int gre, int roc) {
+        this.kind = kd;
+        this.startCost.Add("cotton", 0);
+        this.startCost.Add("food", 0);
+        this.startCost.Add("metal", 0);
+        this.startCost.Add("gold", 0);
+        this.peopleEmployed = 1;
+
+    }
+
+    public void defineCost () {
+        //* TODOS *//
+        // for a given kind, assign some pre defined values for starting costs, 
+        
+        //as well as production costs 
+        
+        //and outputs
+
+
+        //assign pollution values
+        if (this.kind == "factory"){
+            pollutionMade = 2;
+        }
+        else if (this.kind == "farm"){
+            pollutionMade = 1;
+        }
+        if (this.kind == "park"){
+            pollutionMade = -1;
+        }
+        
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
